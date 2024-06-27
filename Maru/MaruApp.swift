f3c7739e4e76f27fe6ca4f6dd7f1b4c9b01415a8
JP Whitaker <jpw@hawaii.edu>
2024-06-26 17:23:18 -1000

    Initial Commit

---
+++ Maru/MaruApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaruApp.swift
+//  Maru
+//
+//  Created by John-Patrick Whitaker on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MaruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
