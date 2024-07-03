f03ce18678936f1ca0602a9c45cfa57dbdd8db9f
JP Whitaker <jpw@hawaii.edu>
2024-07-03 13:52:47 -1000

    tutorial 2 30,45,60

--- Maru/Views/Tutorial/TutorialView02.swift
+++ Maru/Views/Tutorial/TutorialView02.swift
@@ -19,12 +19,18 @@ struct TutorialView02: View {
   
   var body: some View {
     VStack {
-      Text("The Unit Circle")
-        .font(.largeTitle)
-        .padding()
       
       Text("Next, starting from Quadrant I, we'll add lines for 30°, 45°, and 60°.  Continue these lines across the circle.")
         .padding()
+      
+      CircleContainer(options: UnitCircleOptions(
+        showLabels: false,
+        onlyAngles: [(0,180), (30,210), (45,225), (60,240), (90,270)],
+        onlyDots: [30, 45, 60, 210, 225, 240]
+      ))
+        .environmentObject(UnitCircleViewModel())
+        .padding()
+      
       Text("In the unit circle, angles like 30°, 45°, and 60° are special because they match easy-to-remember points. These points come from simple shapes like equilateral and right triangles. , which have well-known properties.")
         .padding()
       
